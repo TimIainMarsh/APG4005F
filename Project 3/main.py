751580e6857a3b9468b4fb2758568d6f665d7569
TimIainMarsh <Timiainmarsh@gmail.com>
2015-03-04 11:21:49 +0200

    Finiashed Main

--- Project 3/main.py
+++ Project 3/main.py
@@ -12,6 +12,7 @@ import numpy as np
 def DomeGenerate(filename):
     f = open(filename , 'w')
     n = 500
+
     for i in range(n):
         r = 50
         xof = 200
@@ -24,6 +25,8 @@ def DomeGenerate(filename):
         Z = zof +  r * mt.cos(ang2)+ random.randint(-100,100)/100.0
         line = str(X) + "," + str(Y)  + "," + str(Z) + '\n'
         f.write(line)
+
+
         
 def dist(Xp,Xc,Yp,Yc):
     return mt.sqrt((Xp-Xc)**2 + (Yp-Yc)**2)        
@@ -42,28 +45,65 @@ if __name__ == '__main__':
 
     Xo,Yo,Zo = 202.0,302.0,102.0
     Ro = 52.0
+    for i in range (4):
+        print ("start it")
+        A =  np.matrix([[0,0,0,0],])
+        A = np.delete(A,0, axis=0)
+        
+        B =  np.zeros(shape=(len(Points),len(Points)*3))
+        
+        P = P = np.matrix(np.identity(len(Points)*3))
+        
+        w =  np.matrix([[0],])
+        w = np.delete(w,0, axis=0)
+        
+        vert = 0
+        hor = 0
     
-    A =  np.matrix([[0,0,0,0],])
-    A = np.delete(A,0, axis=0)
+        for row in range(len(Points)):
+    #        print (Points[row+1])
+            
+            Xp = float(Points[row+1].x)
+            Yp = float(Points[row+1].y)
+            Zp = float(Points[row+1].z)
+            
+            Ax = -2*(Xp-Xo)
+            Ay = -2*(Yp-Yo)
+            Az = -2*(Zp-Zo)
+            Ar = -2*Ro
+            
+            Bx =  2*(Xp-Xo)
+            By =  2*(Yp-Yo)
+            Bz =  2*(Zp-Zo)
+            
+            Arow = [Ax , Ay, Az, Ar]
+            A = np.vstack([A,Arow])
+        
+        
+            c =(Xp - Xo)**2 + (Yp - Yo)**2 + (Zp - Zo)**2 - Ro**2
+            w = np.vstack([w,c])
+        
+            B[vert][hor] = Bx
+            B[vert][hor+1] = By
+            B[vert][hor+2] = Bz
+            vert += 1
+            hor += 3
+        
+        
+        Q = (B * P.I * B.T).I
+        
+        X = -(A.T * Q * A).I * A.T * Q * w
+
+        K = - Q * (A*X + w)
+        
+        V = P.I * B.T * K
     
-    B =  np.zeros(shape=(len(Points),len(Points)*3))
+        Xo = Xo + float(X[0])
+        Yo = Yo + float(X[1])
+        Zo = Zo + float(X[2])
+        Ro = Ro + float(X[3])
     
-    
-    
-    w =  np.matrix([[0],])
-    w = np.delete(w,0, axis=0)
-    
-    
-    for row in range(len(Points)):
-        print(1)
-    
-    
-    
-    
-    
-    
-    
-    
+    print (Xo,Yo,Zo,Ro)  
     
     
     
