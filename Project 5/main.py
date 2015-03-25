bf247e9bb7027111e25f018c671c9e1bca4211ed
TimIainMarsh <Timiainmarsh@gmail.com>
2015-03-25 12:05:53 +0200

    added iterations

--- Project 5/main.py
+++ Project 5/main.py
@@ -49,130 +49,124 @@ if __name__ == '__main__':
 #    print (Points)
     #print (Obs)
     
-    
-    
-    
-    unknowns = []
-    for Point_name, P in Points.items():
-        if P.Tag == 'P':
-            unknowns.append(Point_name + '_x')
-            unknowns.append(Point_name + '_y')
-    unknowns.sort()
-
-    A = np.zeros(shape=(len(unknowns),len(Obs)))
-    P = np.array(np.identity(len(Obs)))
-    L = []
-    
-#    print(len(A),len(A[0]))
-#    print(len(unknowns))
-#    print(len(Obs))
-#    print (unknowns)
-    acc = 0
-    for unknown in unknowns:
-        down = 0
-        UNsp = unknown.split('_')     #UNsp[0] = name ,UNsp[0] = X or Y
-#        print (UNsp[0])
-        for obs_name,obs in Obs.items():
-            
-            OBsp = obs_name.split('-')
-#            print (UNsp[0],UNsp[1],OBsp[0],OBsp[1])
-            if OBsp[0] == UNsp[0]:
-#                print('1')
-
-                point = Points[OBsp[0]]
-                target = Points[OBsp[1]]
-                
-                dx1,dx2,dy1,dy2 = syp.symbols('dx1 dx2 dy1 dy2', real=True)
-                
-                x1,x2,y1,y2 = syp.symbols('x1 x2 y1 y2', real=True)
-                
-                a = syp.Function('a')
-                a = (x2 - x1)/dist(target.x,target.y,point.x,point.y)
-                
-                b = syp.Function('b')
-                b = (y2 - y1)/dist(target.x,target.y,point.x,point.y)
-
-                f = syp.Function('f')
-                f = a*(dy1 - dy2) - b*(dx1 - dx2)
-                if UNsp[1] == 'x':
-                    valueX = f.diff(dx1)
-                    valueX = valueX.subs(y2,target.y).subs(y1,point.y)
-#                    print (valueX)
-#                    print (col,row)
-                    A[acc][down] = valueX
-                    
-                if UNsp[1] == 'y':
-                    valueY = f.diff(dy1)
-                    valueY = valueY.subs(x2,target.x).subs(x1,point.x)
-#                    print (valueY)
-#                    print (col,row)
-                    A[acc][down] = valueY
-                    
-                    
-            if OBsp[1] == UNsp[0]:
-#                print('2')
-#                print (UNsp[0],UNsp[1],OBsp[0],OBsp[1])
-                point = Points[OBsp[0]]
-                target = Points[OBsp[1]]
-#                print (point.x,point.y,target.x,target.y)
-                dx1,dx2,dy1,dy2 = syp.symbols('dx1 dx2 dy1 dy2', real=True)
-                
-                x1,x2,y1,y2 = syp.symbols('x1 x2 y1 y2', real=True)
-                
-                a = syp.Function('a')
-                a = (x2 - x1)/dist(target.x,target.y,point.x,point.y)
-                
-                b = syp.Function('b')
-                b = (y2 - y1)/dist(target.x,target.y,point.x,point.y)
-
-                f = syp.Function('f')
-                f = a*(dy1 - dy2) - b*(dx1 - dx2)
-                if UNsp[1] == 'x':
-#                    print('adding')
-                    valueX = f.diff(dx2)
-#                    print (valueX)
-#                    print (target.y,point.y)
-                    valueX = valueX.subs(y2,target.y).subs(y1,point.y)
-#                    print (valueX)
-#                    print (col,row)
-                    A[acc][down] = valueX
-                    
-                if UNsp[1] == 'y':
-                    
-                    valueY = f.diff(dy2)
-#                    print (valueY)
-                    valueY = valueY.subs(x2,target.x).subs(x1,point.x)
-#                    print (valueY)
-#                    print (col,row)
-                    A[acc][down] = valueY
-                    
-#                print(acc,down)
-            down += 1
-        acc += 1
         
-    row = 0
-    for obs_name,obs in Obs.items():
-        OBsp = obs_name.split('-')
-        fr = Points[OBsp[0]]
-        to = Points[OBsp[1]]
-        L.append(float(obs.Direction) - float(join(to,fr,obs)))
-
-    L = (np.asmatrix(L)).T
-    A = (np.asmatrix(A)).T
+    for i in range(10):      #number of iterations
+        unknowns = []
+        for Point_name, P in Points.items():
+            if P.Tag == 'P':
+                unknowns.append(Point_name + '_x')
+                unknowns.append(Point_name + '_y')
+        unknowns.sort()
     
-    X = (A.T * P * A).I * A.T*P*L
-    print (X)
+        A = np.zeros(shape=(len(unknowns),len(Obs)))
+        P = np.array(np.identity(len(Obs)))
+        L = []
 
-    count = 0
-    for i in unknowns:
-        sp = i.split('_')
-        point = Points[sp[0]]
-        if sp[1] == 'x':
-            point.updateX(float(X[count]))
-        if sp[1] == 'y':
-            point.updateY(float(X[count]))
-        count +=1
+        acc = 0
+        for unknown in unknowns:
+            down = 0
+            UNsp = unknown.split('_')     #UNsp[0] = name ,UNsp[0] = X or Y
+            for obs_name,obs in Obs.items():
+                OBsp = obs_name.split('-')
+                if OBsp[0] == UNsp[0]:
 
+                    point = Points[OBsp[0]]
+                    target = Points[OBsp[1]]
+                    
+                    dx1,dx2,dy1,dy2 = syp.symbols('dx1 dx2 dy1 dy2', real=True)
+                    
+                    x1,x2,y1,y2 = syp.symbols('x1 x2 y1 y2', real=True)
+                    
+                    a = syp.Function('a')
+                    a = (x2 - x1)/dist(target.x,target.y,point.x,point.y)
+                    
+                    b = syp.Function('b')
+                    b = (y2 - y1)/dist(target.x,target.y,point.x,point.y)
+    
+                    f = syp.Function('f')
+                    f = a*(dy1 - dy2) - b*(dx1 - dx2)
+                    if UNsp[1] == 'x':
+                        valueX = f.diff(dx1)
+                        valueX = valueX.subs(y2,target.y).subs(y1,point.y)
+    #                    print (valueX)
+    #                    print (col,row)
+                        A[acc][down] = valueX
+                        
+                    if UNsp[1] == 'y':
+                        valueY = f.diff(dy1)
+                        valueY = valueY.subs(x2,target.x).subs(x1,point.x)
+    #                    print (valueY)
+    #                    print (col,row)
+                        A[acc][down] = valueY
+                        
+                        
+                if OBsp[1] == UNsp[0]:
+    #                print('2')
+    #                print (UNsp[0],UNsp[1],OBsp[0],OBsp[1])
+                    point = Points[OBsp[0]]
+                    target = Points[OBsp[1]]
+    #                print (point.x,point.y,target.x,target.y)
+                    dx1,dx2,dy1,dy2 = syp.symbols('dx1 dx2 dy1 dy2', real=True)
+                    
+                    x1,x2,y1,y2 = syp.symbols('x1 x2 y1 y2', real=True)
+                    
+                    a = syp.Function('a')
+                    a = (x2 - x1)/dist(target.x,target.y,point.x,point.y)
+                    
+                    b = syp.Function('b')
+                    b = (y2 - y1)/dist(target.x,target.y,point.x,point.y)
+    
+                    f = syp.Function('f')
+                    f = a*(dy1 - dy2) - b*(dx1 - dx2)
+                    if UNsp[1] == 'x':
+    #                    print('adding')
+                        valueX = f.diff(dx2)
+    #                    print (valueX)
+    #                    print (target.y,point.y)
+                        valueX = valueX.subs(y2,target.y).subs(y1,point.y)
+    #                    print (valueX)
+    #                    print (col,row)
+                        A[acc][down] = valueX
+                        
+                    if UNsp[1] == 'y':
+                        
+                        valueY = f.diff(dy2)
+    #                    print (valueY)
+                        valueY = valueY.subs(x2,target.x).subs(x1,point.x)
+    #                    print (valueY)
+    #                    print (col,row)
+                        A[acc][down] = valueY
+                        
+    #                print(acc,down)
+                down += 1
+            acc += 1
+            
+        row = 0
+        for obs_name,obs in Obs.items():
+            OBsp = obs_name.split('-')
+            fr = Points[OBsp[0]]
+            to = Points[OBsp[1]]
+            L.append(float(obs.Direction) - float(join(to,fr,obs)))
+    
+        L = (np.asmatrix(L)).T
+        A = (np.asmatrix(A)).T
+        
+        X = (A.T * P * A).I * A.T*P*L
+#        print (X)
+        V = A*X - L
+        print (V)
+        count = 0
+        for i in unknowns:
+            sp = i.split('_')
+            point = Points[sp[0]]
+            if sp[1] == 'x':
+                point.updateX(float(X[count]))
+            if sp[1] == 'y':
+                point.updateY(float(X[count]))
+            count +=1
+
+    for pn,p in Points.items():
+        print (pn,p.x,p.y, p.Tag)
 
 
 
